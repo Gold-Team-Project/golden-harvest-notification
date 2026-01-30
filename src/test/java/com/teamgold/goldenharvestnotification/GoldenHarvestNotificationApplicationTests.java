63da19441b20a65c10811da27016292714eb253f
kang seong hyun <127469438+okok02110211@users.noreply.github.com>
2026-01-30 14:50:10 +0900

    첫 커밋

---
+++ src/test/java/com/teamgold/goldenharvestnotification/GoldenHarvestNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamgold.goldenharvestnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenHarvestNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
